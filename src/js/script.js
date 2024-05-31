ecf5c5812a1df5289695c3379219d8a93b5b808d
rodolfo-brand <rnbrandalise@gmail.com>
2024-05-31 01:53:25 -0300

    primeira versão do código teste

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,17 @@
-import codigos from './codigos.js';
-const botoes = document.getElementById('botoes');
-codigos.forEach(i => botoes.innerHTML += `<button class="botao"><img src="src/imagens/bau.png" alt=${i}></button>`);
-botoes.addEventListener('click', (event) => copyToClipboard(event.target.alt));
+const buttonContainer = document.getElementById('button-container');
 
-function copyToClipboard(codigo) {
-
-    const clickedButton = document.querySelector('.botao:focus');
-    if (clickedButton && codigo) {
-        navigator.clipboard.writeText(codigo);
-        return clickedButton.classList.add('botao-clicado')
-    };
-}
+buttonContainer.addEventListener('click', async (event) => {
+  const clickedButton = event.target;  // Get the clicked element
+  
+  if (clickedButton.tagName === 'BUTTON') { // Check if it's a button
+    const buttonId = clickedButton.id.split('-')[1]; // Extract text element ID
+    
+    try {
+      await navigator.clipboard.writeText(clickedButton.textContent);
+      clickedButton.classList.add('botao-clicado'); // Add class after successful copy
+    } catch (err) {
+      console.error('Error copying text:', err);
+      alert('Oops! Something went wrong while copying the text.');
+    }
+  }
+});
